c521171b2559c3f1eb5d7485f40786c5167f1e4b
kharne8 <cerasta81@gmail.com>
2022-01-06 14:37:08 -0600

    clock completed

--- Day 19 Clock App/script.js
+++ Day 19 Clock App/script.js
@@ -1,8 +1,8 @@
 const hour = document.querySelector('.hour');
 const minute = document.querySelector('.minute');
 const second = document.querySelector('.second');
-const time = document.querySelector('.time');
-const date = document.querySelector('.date');
+const timeEl = document.querySelector('.time');
+const dateEl = document.querySelector('.date');
 const toggle = document.querySelector('.toggle');
 
 const days = [
@@ -41,3 +41,50 @@ toggle.addEventListener('click', (e) => {
 		e.target.innerHTML = 'Light Mode';
 	}
 });
+
+function setTime() {
+	const time = new Date();
+	const date = time.getDate();
+	const month = time.getMonth();
+	const day = time.getDay();
+	const hours = time.getHours();
+	const hoursClock = hours % 12;
+	const minutes = time.getMinutes();
+	const seconds = time.getSeconds();
+	const ampm = hours >= 12 ? 'PM' : 'AM';
+
+	hour.style.transform = `translate(-50%, -100%) rotate(${scale(
+		hoursClock,
+		0,
+		12,
+		0,
+		360
+	)}deg)`;
+	minute.style.transform = `translate(-50%, -100%) rotate(${scale(
+		minutes,
+		0,
+		60,
+		0,
+		360
+	)}deg)`;
+	second.style.transform = `translate(-50%, -100%) rotate(${scale(
+		seconds,
+		0,
+		60,
+		0,
+		360
+	)}deg)`;
+
+	timeEl.innerHTML = `${hoursClock}:${
+		minutes < 10 ? `0 ${minutes}` : minutes
+	}:${seconds} ${ampm}`;
+	dateEl.innerHTML = `${days[day]}, ${months[month]} <span class='circle'>${date}</span>`;
+}
+
+function scale(number, inMin, inMax, outMin, outMax) {
+	return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
+
+setTime();
+
+setInterval(setTime, 1000);
