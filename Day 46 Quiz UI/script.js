8556f2ec1bfbb4b970d8bf1213c505bdb3ee16a9
kharne8 <cerasta81@gmail.com>
2022-02-22 10:45:33 -0600

    added quiz API

--- Day 46 Quiz UI/script.js
+++ Day 46 Quiz UI/script.js
@@ -1,41 +1,5 @@
-// token ATsDBB8W1rIuzb3zTjcOPMgaykjSzcmkom7kdRhN
-const quizData = [
-	{
-		question: 'Which language runs in a web browser?',
-		a: 'Java',
-		b: 'C',
-		c: 'Python',
-		d: 'JavaScript',
-		correct: 'd',
-	},
-	{
-		question: 'What does CSS stand for?',
-		a: 'Central Style Sheets',
-		b: 'Cascading Style Sheets',
-		c: 'Cascading Simple Sheets',
-		d: 'Cars SUVs Sailboats',
-		correct: 'b',
-	},
-	{
-		question: 'What does HTML stand for?',
-		a: 'Hypertext Markup Language',
-		b: 'Hypertext Markdown Language',
-		c: 'Hyperloop Machine Language',
-		d: 'Helicopters Terminals Motorboats Lamborginis',
-		correct: 'a',
-	},
-	{
-		question: 'What year was JavaScript launched?',
-		a: '1996',
-		b: '1995',
-		c: '1994',
-		d: 'none of the above',
-		correct: 'b',
-	},
-];
-
 const quiz = document.getElementById('quiz');
-const answersEl = document.querySelectorAll('.answer');
+const answerEls = document.querySelectorAll('.answer');
 const questionEl = document.getElementById('question');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
@@ -45,32 +9,67 @@ const btn = document.getElementById('submit');
 
 let currentQuiz = 0;
 let score = 0;
+let quizData = [];
 
-loadQuiz();
+fetchQuiz();
+
+async function fetchQuiz() {
+	const res = await fetch(
+		'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
+	);
+
+	const data = await res.json();
+	data.results.forEach((data) => {
+		const mold = { question: '', answers: [], correct: '' };
+		mold.question = data.question;
+		for (let i = 0; i < data.incorrect_answers.length; i++) {
+			mold.answers.push(data.incorrect_answers[i]);
+		}
+		mold.answers.push(data.correct_answer);
+		mold.correct = data.correct_answer;
+
+		quizData.push(mold);
+	});
+
+	loadQuiz();
+}
 
 function loadQuiz() {
+	let nando = [];
+
+	while (nando.length < 4) {
+		let hold = Math.floor(Math.random() * 4);
+		if (nando.includes(hold)) {
+		} else {
+			nando.push(hold);
+		}
+	}
+
 	deselectAnswers();
 
 	const currentQuizData = quizData[currentQuiz];
 
 	questionEl.innerText = currentQuizData.question;
-	a_text.innerText = currentQuizData.a;
-	b_text.innerText = currentQuizData.b;
-	c_text.innerText = currentQuizData.c;
-	d_text.innerText = currentQuizData.d;
+	a_text.innerText = currentQuizData.answers[nando[0]];
+	b_text.innerText = currentQuizData.answers[nando[1]];
+	c_text.innerText = currentQuizData.answers[nando[2]];
+	d_text.innerText = currentQuizData.answers[nando[3]];
+	console.log(currentQuizData);
 }
 
 function deselectAnswers() {
-	answersEl.forEach((answerEl) => (answerEl.checked = false));
+	answerEls.forEach((answerEl) => (answerEl.checked = false));
 }
 
 function getSelected() {
-	let answer = null;
-	answersEl.forEach((answerEl) => {
+	let answer;
+
+	answerEls.forEach((answerEl) => {
 		if (answerEl.checked) {
-			answer = answerEl.id;
+			answer = answerEl.nextElementSibling.innerText;
 		}
 	});
+
 	return answer;
 }
 
@@ -81,15 +80,16 @@ btn.addEventListener('click', () => {
 		if (answer === quizData[currentQuiz].correct) {
 			score++;
 		}
+
 		currentQuiz++;
 
 		if (currentQuiz < quizData.length) {
 			loadQuiz();
 		} else {
 			quiz.innerHTML = `
-			<h2>You answered ${score}/${quizData.length} questions correctly</h2>
-			<button onClick='location.reload()'>Reload</button>
-			`;
+                <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+                <button onclick="location.reload()">Reload</button>
+            `;
 		}
 	}
 });
