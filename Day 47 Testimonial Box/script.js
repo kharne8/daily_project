f203c65982c9f728b5402d3931b3fecda55a0b52
kharne8 <cerasta81@gmail.com>
2022-02-22 19:15:41 -0600

    testimonial UI working with API

---
+++ Day 47 Testimonial Box/script.js
@@ -0,0 +1,44 @@
+const testContainer = document.querySelector('.testimonial-container');
+const testimonial = document.querySelector('.testimonial');
+const userImg = document.querySelector('.user-image');
+const userName = document.querySelector('.user-name');
+const role = document.querySelector('.role');
+
+const testimonials = [];
+
+fetchData();
+
+async function fetchData() {
+	const res = await fetch('https://testimonialapi.toolcarton.com/api');
+
+	const data = await res.json();
+	data.forEach((element) => {
+		const mold = { name: '', position: '', photo: '', text: '' };
+		mold.name = element.name;
+		mold.position = element.designation;
+		mold.photo = element.avatar;
+		mold.text = element.message;
+
+		testimonials.push(mold);
+	});
+	console.log(testimonials[0]);
+}
+
+let idx = 1;
+
+function nextTestimonial() {
+	const { name, position, photo, text } = testimonials[idx];
+	const rando = () => Math.floor(Math.random() * 100);
+	testimonial.innerHTML = text;
+	userImg.src = photo;
+	userName.innerHTML = name;
+	role.innerHTML = position;
+
+	idx++;
+
+	if (idx > testimonials.length - 1) {
+		idx = 0;
+	}
+}
+
+setInterval(() => nextTestimonial(), 5000);
